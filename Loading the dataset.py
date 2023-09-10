b7bc41f48029e19ecfaf2a558250a27f3dfedf76
sassayed <67038017+sassayed@users.noreply.github.com>
2023-09-10 06:15:21 +0400

    Update and rename ReqLib.py to Loading the dataset.py

--- Loading the dataset.py
+++ Loading the dataset.py
@@ -4,3 +4,5 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+# load the datase
+df = pd.read_csv(r"C:\Users\MOPA\ML-1\Chatbot\Speech\QA.csv", encoding_errors='ignore')
