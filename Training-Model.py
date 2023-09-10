3e3a9ce960687c25491e59510df2ac0d8171e536
sassayed <67038017+sassayed@users.noreply.github.com>
2023-09-10 06:22:58 +0400

    Create Training-Model.py

---
+++ Training-Model.py
@@ -0,0 +1,25 @@
+# Training Model
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Embedding, LSTM, Input
+enc_inp = Input(shape=(13, ))  # 13 feartures for input sequense
+dec_inp = Input(shape=(13, ))# 13 feartures for output sequense
+
+VOCAB_SIZE = len(vocab)
+embed = Embedding(VOCAB_SIZE+1, output_dim=50, 
+                  input_length=13,
+                  trainable=True                  
+                  )
+
+enc_embed = embed(enc_inp)
+enc_lstm = LSTM(400, return_sequences=True, return_state=True)
+enc_op, h, c = enc_lstm(enc_embed)
+enc_states = [h, c]
+
+dec_embed = embed(dec_inp)
+dec_lstm = LSTM(400, return_sequences=True, return_state=True)
+dec_op, _, _ = dec_lstm(dec_embed, initial_state=enc_states)
+
+dense = Dense(VOCAB_SIZE, activation='softmax')
+dense_op = dense(dec_op)
+model = Model([enc_inp, dec_inp], dense_op)
+model.compile(loss='categorical_crossentropy',metrics=['acc'],optimizer='adam')
